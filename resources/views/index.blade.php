89cf889609d98754d27dde98b1c1e04de2899fb3
bencepiatrik <bence.piatrik@azet.sk>
2025-01-28 14:57:13 +0100

    in progress

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+    <div>There are tasks!</div>
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id'=>$task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+@endsection
