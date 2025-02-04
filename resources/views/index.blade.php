0cb32fac9dd2523d05f9decde16e2514678a9711
bencepiatrik <bence.piatrik@azet.sk>
2025-02-04 23:20:33 +0100

    pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,4 +11,9 @@
     @empty
         <div>There are no tasks!</div>
     @endforelse
+    @if($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
 Route::get('/tasks', function (){
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->get()
+        'tasks' => \App\Models\Task::latest()->paginate()
     ]);
 })->name('tasks.index');
 
